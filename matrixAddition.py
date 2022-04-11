bb840a6558d0ec963d3b4658bf483b2f4bcd925b
Onur Koçak <98240992+onurkck34@users.noreply.github.com>
2022-04-12 01:28:55 +0300

    Update matrixAddition.py

--- matrixAddition.py
+++ matrixAddition.py
@@ -4,5 +4,5 @@ y = [[3,3,4],[2,4,1],[3,5,4]]
 result = [[0,0,0],[0,0,0],[0,0,0]]
 for i in range(len(x)):
     for j in range(len(y)):
-        result[i][j] = x[i][j]+y[i][j]
+        
 print(result)
